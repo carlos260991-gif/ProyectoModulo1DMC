3714cb616f8880298658d601d3449ee367027539
carlos260991-gif <carlos260991@gmail.com>
2026-07-14 00:04:53 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals") #title par crer un titulo en un espacio generado#
 st.sidebar.title ("Parámetros") #sidebar se usa para crear un adicional#
+#ahora crearemos una interfaz insertando imagenes#
+st.image(Python logo.png)
+
 # a continuación se genera tres variables que generan una lista numerica que se visualizarán como un desplegable numerico de los rango determinados#
 valor_inicial = st.number_input("ingrese el valor inicial", value = 0)
 valor_final = st.number_input("ingrese el valor final",value = 1)
