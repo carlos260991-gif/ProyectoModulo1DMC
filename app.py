495d56225e3f63f25e91937357547fec7c032546
carlos260991-gif <carlos260991@gmail.com>
2026-07-13 23:55:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Proyecto módulo 1 Fundamentals")
-st.sidebar.title ("Parámetros")
+st.sidebar.title ("Parámetros") #sidebar se usa para crear un adicional#
 
 valor_inicial = st.number_input("ingrese el valor inicial", value = 0)
 valor_final = st.number_input("ingrese el valor final",value = 1)
